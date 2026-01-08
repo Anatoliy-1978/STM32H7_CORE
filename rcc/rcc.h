3d3cf136c8cecbedd1f4129f9e105e2d28522ef1
Anatoliy-1978 <bykanovanatoliy@ya.ru>
2026-01-08 22:51:03 +0300

    Create rcc.h

---
+++ rcc/rcc.h
@@ -0,0 +1,19 @@
+/*
+ * rcc.h   Board STM32H7_CORE
+ * 03.01.2026
+ */
+#ifndef __RCC_H__
+#define __RCC_H__
+
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
+#include "stm32h7xx.h"
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // __RCC_H__
