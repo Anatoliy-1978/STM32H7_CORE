e3ce579d917f95edc9ece6bbbaab891ae1e145dc
Anatoliy-1978 <bykanovanatoliy@ya.ru>
2026-01-18 10:56:33 +0300

    Create tim6_7.h

---
+++ tim6_7.h
@@ -0,0 +1,21 @@
+/*
+ * tim6_7.h   Board STM32H7_CORE
+ * 03.01.2026
+ */
+#ifndef __TIM6_7_H__
+#define __TIM6_7_H__
+
+#ifdef __cplusplus
+ extern "C" {
+#endif
+
+#include "stm32h7xx.h"
+#include <stdint.h>
+
+
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif // __TIM6_7_H__
